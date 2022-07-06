a44a6a0dbf1d16a30fe3d464806f814ebf41524b
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-07-06 11:15:20 -0300

    Upload Example IntentDigitalHUB FireMonkey

---
+++ Exemplos/App_IntentDigitalHUB_FireMonkey/Elgin_IntentDigitalHUB_Flutter-font/Sat.pas
@@ -0,0 +1,350 @@
+unit Sat;
+
+interface
+
+uses
+  Androidapi.JNI.GraphicsContentViewText,
+  Androidapi.Helpers,
+  Androidapi.JNI.JavaTypes,
+  System.Classes,
+  System.IOUtils,
+  System.Messaging,
+  Androidapi.Jni.App,
+  FMX.Types,
+  System.Math,
+  AndroidAPI.JNIBridge,
+  System.SysUtils,
+  FMX.Dialogs;
+
+type
+  TCallBack = reference to procedure(msg: string = '');
+
+  TSat = class
+    RetornoCallBack       : TCallBack;
+    FMessageSubscriptionID: integer;
+    private
+      { private declarations }
+      function FileToString(arquivo: string): string;
+      procedure HandleActivityMessage(const Sender: TObject;const msg: TMessage);
+      procedure respostaBridge(dados: JIntent);
+      procedure longlong(str: string);
+    function createFileFromInputStream(input: JInputStream; xml: string): JFile;
+    protected
+      { protected declarations }
+    public
+    { public declarations }
+    procedure ConsultarSat(Retorno: TCallBack);
+    procedure CancelarUltimaVenda(codAtivacao, numeroCFe,dadosCancelamento: string;
+      Retorno: TCallBack);
+    procedure ConsultarStatusOperacional(codAtivacao: String; Retorno: TCallBack);
+    procedure AtivarSat(subComando, codAtivacao, cnpj, cUF: string; Retorno: TCallBack);
+    procedure EnviarDadosVenda(codAtivacao, dadosVenda: String; Retorno: TCallBack);
+    procedure AssociarAssinatura(codAtivacao, cnpjSH, assinaturaAC: String; Retorno: TCallBack);
+    procedure ExtrairLogs(codAtivacao: String; Retorno: TCallBack);
+//      function trocarCodigoDeAtivacao(numSessao: Integer; codAtivacao: String; opcao: Integer; novoCodigo: String; confNovoCodigo: String): String;
+  end;
+
+var
+  sat_instance: TSat;
+
+implementation
+
+{ TSat }
+
+function TSat.FileToString(arquivo: string): string;
+var
+      TextFile: TStringList;
+begin
+
+  arquivo := System.IOUtils.TPath.GetDocumentsPath + PathDelim + arquivo;
+  TextFile := TStringList.Create;
+  try
+    try
+      TextFile.LoadFromFile(arquivo);
+
+      Result := TextFile.Text;
+
+    finally
+      FreeAndNil(TextFile);
+    end
+  except
+
+    on E:Exception do ShowMessage('Não foi possível abrir o arquivo!');
+  end;
+end;
+
+procedure TSat.AssociarAssinatura(codAtivacao, cnpjSH, assinaturaAC: String;
+  Retorno: TCallBack);
+var
+  IntentBridge: JIntent;
+  comando: string;
+  numSessao: integer;
+begin
+   RetornoCallBack := Retorno;
+   numSessao := RandomRange(1,1000);
+
+   IntentBridge := TJIntent.JavaClass.init(StringToJString('com.elgin.e1.digitalhub.SAT'));
+
+   comando:='[{"funcao":"AssociarAssinatura",'+
+                  '"parametros":{"numSessao":'+ IntToStr(numSessao) +','+
+                  '"codAtivacao":"'+ codAtivacao +'",'+
+                  '"cnpjSH":"'+ cnpjSH +'",'+
+                  '"assinaturaAC":"'+ assinaturaAC +'"}}]';
+
+   IntentBridge.putExtra(StringToJString('comando'), StringToJString(comando));
+   
+   FMessageSubscriptionID := TMessageManager.DefaultManager.SubscribeToMessage(TMessageResultNotification, HandleActivityMessage);
+   TAndroidHelper.Activity.startActivityForResult(IntentBridge, 4321);
+end;
+
+procedure TSat.AtivarSat(subComando, codAtivacao, cnpj, cUF: string;
+  Retorno: TCallBack);
+var
+  IntentBridge: JIntent;
+  comando: string;
+  numSessao: integer;
+begin
+   RetornoCallBack := Retorno;
+   numSessao := RandomRange(1,1000);
+
+   IntentBridge := TJIntent.JavaClass.init(StringToJString('com.elgin.e1.digitalhub.SAT'));
+
+   comando:='[{"funcao":"AtivarSAT",'+
+                  '"parametros":{"numSessao":'+ IntToStr(numSessao) +','+
+                  '"subComando":'+ subComando +','+
+                  '"codAtivacao":"'+ codAtivacao +'",'+
+                  '"cnpj":"'+ cnpj +'",'+
+                  '"cUF":'+ cUF +'}}]';
+
+   IntentBridge.putExtra(StringToJString('comando'), StringToJString(comando));
+
+   FMessageSubscriptionID := TMessageManager.DefaultManager.SubscribeToMessage(TMessageResultNotification, HandleActivityMessage);
+   TAndroidHelper.Activity.startActivityForResult(IntentBridge, 4321);
+end;
+
+procedure TSat.CancelarUltimaVenda(codAtivacao, numeroCFe,
+  dadosCancelamento: string; Retorno: TCallBack);
+var
+  IntentBridge: JIntent;
+  comando, dir, xml: string;
+  numSessao: integer;
+  file_output:JFile;
+  input: JInputStream;
+begin
+//   dir:= TPath.GetDownloadsPath + TPath.DirectorySeparatorChar + dadosCancelamento;
+//   dir := StringReplace(dir,'/storage/emulated/0','', [rfReplaceAll, rfIgnoreCase]);
+//   log.d('path ' + dir);
+//
+//   file_output:=  TJFile.JavaClass.init(StringToJString(System.IOUtils.TPath.GetDocumentsPath + PathDelim + dadosCancelamento));
+//   input := TJFileInputStream.JavaClass.init(file_output);
+//   file_output:= createFileFromInputStream(input, dadosCancelamento);
+
+   RetornoCallBack := Retorno;
+   numSessao := RandomRange(1,1000);
+
+   IntentBridge := TJIntent.JavaClass.init(StringToJString('com.elgin.e1.digitalhub.SAT'));
+
+   comando:='[{"funcao":"CancelarUltimaVenda",'+
+                  '"parametros":{"numSessao":'+ IntToStr(numSessao) +','+
+                  '"codAtivacao":"'+ codAtivacao +'",'+
+                  '"numeroCFe":"'+ numeroCFe +'",'+
+                  '"dadosCancelamento":"'+ dadosCancelamento +'"}}]';
+
+   IntentBridge.putExtra(StringToJString('comando'), StringToJString(comando));
+
+   log.d('cancelar: ' + comando);
+
+   FMessageSubscriptionID := TMessageManager.DefaultManager.SubscribeToMessage(TMessageResultNotification, HandleActivityMessage);
+   TAndroidHelper.Activity.startActivityForResult(IntentBridge, 4321);
+end;
+
+procedure TSat.ConsultarSat(Retorno: TCallBack);
+var
+  IntentBridge: JIntent;
+  comando: string;
+  numSessao: integer;
+begin
+   RetornoCallBack := Retorno;
+   numSessao := RandomRange(1,1000);
+
+   IntentBridge := TJIntent.JavaClass.init(StringToJString('com.elgin.e1.digitalhub.SAT'));
+
+   comando:='[{"funcao":"ConsultarSat",'+
+                  '"parametros":{"numSessao":'+ IntToStr(numSessao) +'}}]';
+
+   IntentBridge.putExtra(StringToJString('comando'), StringToJString(comando));
+
+   FMessageSubscriptionID := TMessageManager.DefaultManager.SubscribeToMessage(TMessageResultNotification, HandleActivityMessage);
+   TAndroidHelper.Activity.startActivityForResult(IntentBridge, 4321);
+end;
+
+procedure TSat.ConsultarStatusOperacional(codAtivacao: String;
+  Retorno: TCallBack);
+var
+  IntentBridge: JIntent;
+  comando: string;
+  numSessao: integer;
+begin
+   RetornoCallBack := Retorno;
+   numSessao := RandomRange(1,1000);
+
+   IntentBridge := TJIntent.JavaClass.init(StringToJString('com.elgin.e1.digitalhub.SAT'));
+
+   comando:='[{"funcao":"ConsultarStatusOperacional",'+
+                  '"parametros":{"numSessao":'+ IntToStr(numSessao) +','+
+                  '"codAtivacao":"'+ codAtivacao +'"}}]';
+
+   IntentBridge.putExtra(StringToJString('comando'), StringToJString(comando));
+
+   FMessageSubscriptionID := TMessageManager.DefaultManager.SubscribeToMessage(TMessageResultNotification, HandleActivityMessage);
+   TAndroidHelper.Activity.startActivityForResult(IntentBridge, 4321);
+end;
+
+procedure TSat.EnviarDadosVenda(codAtivacao, dadosVenda: String;
+  Retorno: TCallBack);
+var
+  IntentBridge: JIntent;
+  comando, dir: string;
+  numSessao: integer;
+  file_output:JFile;
+  input: JInputStream;
+begin
+   dir:= TPath.GetDownloadsPath + TPath.DirectorySeparatorChar + dadosVenda;
+   dir := StringReplace(dir,'/storage/emulated/0','', [rfReplaceAll, rfIgnoreCase]);
+
+   file_output:=  TJFile.JavaClass.init(StringToJString(System.IOUtils.TPath.GetDocumentsPath + PathDelim + dadosVenda));
+   input := TJFileInputStream.JavaClass.init(file_output);
+   file_output:= createFileFromInputStream(input, dadosVenda);
+   RetornoCallBack := Retorno;
+
+
+   RetornoCallBack := Retorno;
+   numSessao := RandomRange(1,1000);
+
+   IntentBridge := TJIntent.JavaClass.init(StringToJString('com.elgin.e1.digitalhub.SAT'));
+
+   comando:='[{"funcao":"EnviarDadosVenda",'+
+                  '"parametros":{"numSessao":'+ IntToStr(numSessao) +','+
+                  '"codAtivacao":"'+ codAtivacao +'",'+
+                  '"dadosVenda":"path='+ dir +'"}}]';
+
+   IntentBridge.putExtra(StringToJString('comando'), StringToJString(comando));
+
+
+   FMessageSubscriptionID := TMessageManager.DefaultManager.SubscribeToMessage(TMessageResultNotification, HandleActivityMessage);
+   TAndroidHelper.Activity.startActivityForResult(IntentBridge, 4321);
+end;
+
+procedure TSat.ExtrairLogs(codAtivacao: String; Retorno: TCallBack);
+var
+  IntentBridge: JIntent;
+  comando: string;
+  numSessao: integer;
+begin
+   RetornoCallBack := Retorno;
+   numSessao := RandomRange(1,1000);
+
+   IntentBridge := TJIntent.JavaClass.init(StringToJString('com.elgin.e1.digitalhub.SAT'));
+
+   comando:='[{"funcao":"ExtrairLogs",'+
+                  '"parametros":{"numSessao":'+ IntToStr(numSessao) +','+
+                  '"codAtivacao":"'+ codAtivacao +'"}}]';
+
+   IntentBridge.putExtra(StringToJString('comando'), StringToJString(comando));
+
+   FMessageSubscriptionID := TMessageManager.DefaultManager.SubscribeToMessage(TMessageResultNotification, HandleActivityMessage);
+   TAndroidHelper.Activity.startActivityForResult(IntentBridge, 4321);
+end;
+
+procedure TSat.HandleActivityMessage(const Sender: TObject;
+  const msg: TMessage);
+begin
+
+  try
+    if msg is TMessageResultNotification then
+    begin
+      TMessageManager.DefaultManager.Unsubscribe(TMessageResultNotification, FMessageSubscriptionID);
+      FMessageSubscriptionID := 0;
+      if TMessageResultNotification(msg).RequestCode = 4321 then
+      begin
+         if TMessageResultNotification(msg).ResultCode = TJActivity.JavaClass.RESULT_OK then
+          respostaBridge(TMessageResultNotification(msg).Value)
+         else if TMessageResultNotification(msg).ResultCode =  TJActivity.JavaClass.RESULT_CANCELED then
+              respostaBridge(TMessageResultNotification(msg).Value)
+         else
+              ShowMessage('Erro resposta Sat');
+
+      end;
+    end;
+
+  except
+      on e: exception do
+        ShowMessage('Erro resposta Sat: ' + e.Message);
+  end;
+
+end;
+
+procedure TSat.longlong(str: string);
+begin
+  if str.Length > 4000 then
+  begin
+    log.d(str.Substring(0,4000));
+    longlong(str.Substring(4000));
+  end
+  else
+    log.d(str);
+
+end;
+
+procedure TSat.respostaBridge(dados: JIntent);
+var
+  msg : JJSONObject;
+  res : JJSONArray;
+begin
+  if Assigned(dados) then
+  begin
+      log.d('----------------------');
+      log.d(JStringToString(dados.getStringExtra(StringToJString('retorno'))));
+      res:= TJJSONArray.JavaClass.init(dados.getStringExtra(StringToJString('retorno')));
+      msg:= res.getJSONObject(0);
+  end;
+
+   RetornoCallBack(JStringToString(msg.getString(StringToJString('resultado'))));
+end;
+
+function TSat.createFileFromInputStream(input: JInputStream; xml: string): JFile;
+var
+  new_file: JFile;
+  output: JOutputStream;
+  buffer: TJavaArray<Byte>;
+  length: integer;
+  dir :string;
+begin
+  try
+    try
+      dir:= TPath.GetDownloadsPath + TPath.DirectorySeparatorChar + xml;
+      new_file:= TJfile.JavaClass.init(StringToJString(dir));
+      output:= TJFileOutputStream.JavaClass.init(new_file);
+
+      buffer:= TJavaArray<Byte>.Create(1024);
+      length:= input.read(buffer);
+      while (length > 0) do
+      begin
+        output.write(buffer, 0, length);
+        length:= input.read(buffer);
+      end;
+    finally
+      output.close;
+      input.close;
+
+      Result:= new_file;
+    end;
+  except
+    on E:Exception do ShowMessage('Não foi possível trasnferir o buffer da imagem !');
+  end;
+
+end;
+
+initialization
+ sat_instance := TSat.Create;
+end.
---
+++ Exemplos/App_IntentDigitalHUB_FireMonkey/Elgin_IntentDigitalHUB_Flutter-font/SatForm.pas
@@ -0,0 +1,249 @@
+unit SatForm;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types,
+  FMX.Edit, FMX.StdCtrls, FMX.ScrollBox, FMX.Memo, FMX.Objects, FMX.Layouts,
+  FMX.Controls.Presentation,
+  System.IOUtils,
+  Androidapi.Helpers,
+  Androidapi.JNI.JavaTypes,
+  System.NetEncoding;
+
+type
+  TFrmSat = class(TForm)
+    LayoutMain: TLayout;
+    LayoutHeading: TLayout;
+    Titulo: TLabel;
+    Logo: TImage;
+    LayoutContent: TLayout;
+    GridPanelLayout1: TGridPanelLayout;
+    LayoutLeft: TLayout;
+    Rectangle1: TRectangle;
+    Label3: TLabel;
+    Memo1: TMemo;
+    LayoutRight: TLayout;
+    GridPanelLayout2: TGridPanelLayout;
+    rd_smart: TRadioButton;
+    rd_satgo: TRadioButton;
+    Label2: TLabel;
+    LayoutLine: TLayout;
+    Edit1: TEdit;
+    rec_consultar: TRectangle;
+    lbl_consultar: TLabel;
+    rec_cancelar: TRectangle;
+    lbl_cancelamento: TLabel;
+    rec_status: TRectangle;
+    lbl_status: TLabel;
+    rec_ativar: TRectangle;
+    lbl_ativar: TLabel;
+    rec_venda: TRectangle;
+    lbl_realiza_venda: TLabel;
+    rec_associar: TRectangle;
+    lbl_associar: TLabel;
+    rec_extrair_logs: TRectangle;
+    Label4: TLabel;
+    LayoutFooter: TLayout;
+    Label1: TLabel;
+
+    procedure botaoEfeitoMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Single);
+    procedure botaoEfeitoMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Single);
+    procedure Retorno(msg: string);
+    procedure rec_consultarClick(Sender: TObject);
+    procedure rec_cancelarClick(Sender: TObject);
+    procedure rec_statusClick(Sender: TObject);
+    procedure rec_ativarClick(Sender: TObject);
+    procedure rec_vendaClick(Sender: TObject);
+    procedure rec_associarClick(Sender: TObject);
+    procedure rec_extrair_logsClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure rd_satgoChange(Sender: TObject);
+  private
+    function FileToString(arquivo: string): string;
+    { Private declarations }
+  public
+
+    { Public declarations }
+
+
+  end;
+
+var
+  FrmSat: TFrmSat;
+  xmlEnviaDadosVenda:string;
+  xmlCancelamento: string;
+  cfeCancelamento:string;
+  op: string='';
+
+implementation
+
+{$R *.fmx}
+
+uses Sat;
+
+{ TFrmSat }
+
+procedure TFrmSat.botaoEfeitoMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Single);
+begin
+   TRectangle(Sender).Opacity := 0.7;
+end;
+
+procedure TFrmSat.botaoEfeitoMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Single);
+begin
+   TRectangle(Sender).Opacity := 1.0;
+end;
+
+procedure TFrmSat.rd_satgoChange(Sender: TObject);
+begin
+  if rd_smart.IsChecked=true then
+  begin
+    xmlEnviaDadosVenda:= 'xmlenviadadosvendasat.xml';
+    xmlCancelamento:= 'sat_cancelamento.xml';
+//    xmlCancelamento:= 'cupomSatCancelamento.xml';
+  end
+  else
+  begin
+    xmlEnviaDadosVenda:= 'satgo3.xml';
+    xmlCancelamento:='cancelamentosatgo.xml';
+  end;
+
+end;
+
+procedure TFrmSat.rec_associarClick(Sender: TObject);
+begin
+  sat_instance.AssociarAssinatura(Edit1.Text, '16716114000172', 'SGR-SAT SISTEMA DE GESTAO E RETAGUARDA DO SAT', Retorno);
+end;
+
+procedure TFrmSat.rec_ativarClick(Sender: TObject);
+begin
+  sat_instance.AtivarSat('2', Edit1.Text, '14200166000166', '15', Retorno);
+end;
+
+procedure TFrmSat.rec_cancelarClick(Sender: TObject);
+var
+  res, xml, mimo: string;
+  i: integer;
+begin
+  xml := FileToString(xmlCancelamento);
+//  xml := xmlCancelamento;
+  xml := StringReplace(xml,'novoCFe',cfeCancelamento,[rfReplaceAll, rfIgnoreCase]);
+  xml := StringReplace(xml,'"','\"',[rfReplaceAll, rfIgnoreCase]);
+  sat_instance.CancelarUltimaVenda(Edit1.TExt, cfeCancelamento, xml, Retorno);
+end;
+
+procedure TFrmSat.rec_consultarClick(Sender: TObject);
+begin
+  sat_instance.ConsultarSat(Retorno);
+end;
+
+procedure TFrmSat.rec_extrair_logsClick(Sender: TObject);
+begin
+  sat_instance.ExtrairLogs(Edit1.Text, Retorno);
+  op:= 'extrair';
+end;
+
+procedure TFrmSat.rec_statusClick(Sender: TObject);
+begin
+  sat_instance.ConsultarStatusOperacional(Edit1.Text, Retorno);
+end;
+
+procedure TFrmSat.rec_vendaClick(Sender: TObject);
+var
+  xml: string;
+begin
+  op:= 'venda' ;
+  xml:= xmlEnviaDadosVenda;
+  sat_instance.EnviarDadosVenda(Edit1.Text, xml, Retorno);
+end;
+
+procedure TFrmSat.Retorno(msg: string);
+var
+  mimo, dir, retornoCfe: string;
+  i: integer;
+  TextFile: TStringList;
+  charArray : Array[0..0] of Char;
+  cfe : TArray<string>;
+begin
+//  ShowMessage(msg);
+  Log.d('RETORNO: ' + msg);
+
+  Memo1.Lines.Clear;
+  if op = 'extrair' then
+  begin
+    op:= '';
+
+
+//    dir:= 'logs_SAT.txt';
+//    dir:= TPath.GetDownloadsPath + TPath.DirectorySeparatorChar + dir;
+//  //  dir:= TPath.GetTempPath +TPath.DirectorySeparatorChar + dir;
+//
+//    charArray[0] := '|';
+//    msg:= (msg.Split(charArray))[5];
+//    msg:= TNetEncoding.Base64.Decode(msg);
+//    TextFile := TStringList.Create;
+//    TextFile.Text:= msg;
+//    TextFile.SaveToFile(dir);
+
+    if msg <> 'DeviceNotFound' then
+      Memo1.Lines.Add('Log Sat salvo em ');
+
+  end;
+  if op='venda' then
+  begin
+    op:='';
+    cfe:= msg.Split(['|']);
+
+    if Length(cfe)>8 then
+      cfeCancelamento:= cfe[8];
+
+
+  end;
+
+  for i := 0 to length(msg)- 1 do
+  begin
+    mimo:= mimo + msg[i];
+    if i mod 40 = 0 then mimo:= mimo + #13#10;
+  end;
+  Memo1.Lines.Add(mimo);
+
+
+end;
+
+function TFrmSat.FileToString(arquivo: string): string;
+ var
+      TextFile: TStringList;
+begin
+
+  arquivo := System.IOUtils.TPath.GetDocumentsPath + PathDelim + arquivo;
+  TextFile := TStringList.Create;
+  try
+    try
+      TextFile.LoadFromFile(arquivo);
+
+      Result := TextFile.Text;
+
+    finally
+      FreeAndNil(TextFile);
+    end
+  except
+
+    on E:Exception do ShowMessage('Não foi possível abrir o arquivo!');
+  end;
+end;
+
+procedure TFrmSat.FormShow(Sender: TObject);
+begin
+  xmlEnviaDadosVenda:= 'xmlenviadadosvendasat.xml';
+  xmlCancelamento:= 'sat_cancelamento.xml';
+  cfeCancelamento:= '';
+
+  rd_smart.IsChecked:= true;
+end;
+
+end.
